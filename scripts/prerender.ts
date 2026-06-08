87ac997a598cb892557892717ee647976bb3d4a7
fknmiz <150495873+fknmiz@users.noreply.github.com>
2026-06-08 16:33:44 -0400

    Enrich public evidence surface

--- scripts/prerender.ts
+++ scripts/prerender.ts
@@ -6,3 +6,5 @@ const input = JSON.parse(readFileSync("fixtures/modernization-ledger.json", "utf
 mkdirSync("site", { recursive: true });
 writeFileSync("site/index.html", renderPage(input));
 writeFileSync("site/robots.txt", "User-agent: *\nAllow: /\n");
+
+writeFileSync("site/sitemap.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n  <url><loc>https://mizcausevic-dev.github.io/govtech-cobol-modernization-ledger/</loc></url>\n</urlset>\n");
